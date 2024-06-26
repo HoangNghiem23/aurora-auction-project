d180d4a6de25d446fa6e69c0a5b355a11b224d89
lntuan07 <jptdot@gmail.com>
2024-06-26 18:00:22 +0700

    listjewelryinauction

---
+++ src/pages/admin/jewelryinauction/index.jsx
@@ -0,0 +1,79 @@
+import { Table } from "antd";
+import { useEffect, useState } from "react";
+import api from "../../../config/axios";
+
+function JewelryInAuctionList() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await api.get("/auctions");
+      setData(response.data.filter((auction) => auction.isJewelry));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      render: (text) => <img src={text} alt="product" width="50" />,
+    },
+    {
+      title: "Starting Bid",
+      dataIndex: "startingBid",
+      key: "startingBid",
+    },
+    {
+      title: "Bid Step",
+      dataIndex: "bidStep",
+      key: "bidStep",
+    },
+    {
+      title: "Start Time",
+      dataIndex: "startTime",
+      key: "startTime",
+    },
+    {
+      title: "End Time",
+      dataIndex: "endTime",
+      key: "endTime",
+    },
+    {
+      title: "Sold Status",
+      dataIndex: "soldStatus",
+      key: "soldStatus",
+      render: (text) => (text ? "Sold" : "Not Sold"),
+    },
+    {
+      title: "Current Bid Amount",
+      dataIndex: "currentBidAmount",
+      key: "currentBidAmount",
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Jewelry Currently in Auction</h1>
+      <Table dataSource={data} columns={columns} rowKey="id" />
+    </div>
+  );
+}
+
+export default JewelryInAuctionList;
