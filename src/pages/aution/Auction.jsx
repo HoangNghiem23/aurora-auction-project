4aed63110ba5af1d54f4503f06b008fbbb50b262
doit-now <baohg1204@gmail.com>
2024-07-03 18:39:26 +0700

    update

--- src/pages/aution/Auction.jsx
+++ src/pages/aution/Auction.jsx
@@ -12,7 +12,10 @@ import {
 import "./aution.scss";
 import { DollarOutlined, EyeOutlined } from "@ant-design/icons";
 import { useState } from "react";
+import UserSection from "../../components/userSection";
 const { Option } = Select;
+import { Typography } from "antd";
+
 function Auction() {
   const [submit, setSubmit] = useState([]);
   const handleOnFinish = (e) => {
@@ -34,20 +37,25 @@ function Auction() {
   return (
     <section className="auction-page">
       <Row className="auction-page__col">
-        <Col span={12} className="auction-page__box-img">
+        <Col span={11} className="auction-page__box-img">
           <div>
             <Image
+              className="auction-page__box-img--img"
               width={450}
               // src = img của sản phẩm cược
-              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiIpAB9oLzJu8_xIyVftZTkkoxkOUoAPeJkA&s"
             />
           </div>
-          <Descriptions>
-            <Descriptions.Item label="name">Tên món hàng</Descriptions.Item>
-          </Descriptions>
+          <h4 className="img-name" style={{ fontWeight: "100" }}>
+            Tên món hàng: <strong>món hàng</strong>
+          </h4>
         </Col>
-        <Col span={12}>
-          <Card title="Diễn biến cuộc đấu giá">
+        <Col span={8}>
+          <Card
+            title="Diễn biến cuộc đấu giá"
+            className="bet-card"
+            bordered={false}
+          >
             <Descriptions title="User Info">
               <Descriptions.Item label="status" span={24}>
                 status
@@ -59,12 +67,11 @@ function Auction() {
                 money
               </Descriptions.Item>
             </Descriptions>
-          </Card>
-          <Card title="Cược">
+            <h1>Cược</h1>
             <Form
               name="basic"
               labelCol={{
-                span: 4,
+                span: 5,
               }}
               wrapperCol={{
                 span: 12,
@@ -81,11 +88,59 @@ function Auction() {
                   span: 16,
                 }}
               >
-                <Button type="primary" htmlType="submit">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ background: " rgb(37, 35, 44)" }}
+                >
                   Cược
                 </Button>
               </Form.Item>
             </Form>
+          </Card>
+        </Col>
+        <Col span={5}>
+          <Card
+            className="auction-page__realtime"
+            title=""
+            style={{ height: "370px" }}
+          >
+            <h3
+              style={{
+                textAlign: "center",
+                fontSize: "20px",
+                color: "red",
+                fontWeight: "400",
+                margin: "5px",
+              }}
+            >
+              Diễn biến cuộc đấu giá
+            </h3>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-end",
+                height: "280px",
+              }}
+            >
+              <div
+                style={{ overflowY: "auto", padding: "0px 10px 0px 0px" }}
+                className="scroll-bar"
+              >
+                <UserSection />
+                <UserSection isMe />
+                <UserSection />
+                <UserSection />
+                <UserSection />
+                <UserSection />
+                <UserSection />
+                <UserSection />
+                <UserSection />
+                {/* khi nhấn Cược thì isMe */}
+                <UserSection isMe />
+              </div>
+            </div>
           </Card>
         </Col>
       </Row>
